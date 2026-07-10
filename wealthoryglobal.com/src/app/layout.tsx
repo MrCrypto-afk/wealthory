8a8830b2c4d1aff8bcd55cb1f24a03cd2758fefa
Mohit Thakur <mohitthakur@Mohits-MacBook-Pro.local>
2026-07-11 01:58:47 +0530

    feat: Integrate Google Tag Manager (GTM-K8KKZRDD)

--- wealthoryglobal.com/src/app/layout.tsx
+++ wealthoryglobal.com/src/app/layout.tsx
@@ -31,6 +31,9 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning className={`${inter.variable} ${playfair.variable} h-full antialiased`}>
       <body className="min-h-full flex flex-col font-sans transition-colors duration-500 ease-in-out">
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K8KKZRDD" height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe>
+        </noscript>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
@@ -46,6 +49,15 @@ export default function RootLayout({
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'G-9PBSV25Z3K');
+          `}
+        </Script>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-K8KKZRDD');
           `}
         </Script>
       </body>
